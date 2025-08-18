0a111e5616b3cb93e6f979c6fb060836216f7782
mr.Toli <anatolibiru888@gmail.com>
2025-08-18 03:13:52 -0900

    solving again

--- Easy/9_Palindrome_Number.py
+++ Easy/9_Palindrome_Number.py
@@ -1,13 +1,19 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
+    def isPalindrome(x: int) -> bool:
         s = str(x)
-        r = []
-        for i in range(len(s)):
-            if s[i] == s[-(i+1)]:
-                r.append("t")
-            else:
-                r.append("f")
-        if "f" in r:
-            return False
-        else:
-            return True
+        return s == s[::-1]
+
+
+        # inaproprate answer
+        # s = str(x)
+        # r = []
+        # for i in range(len(s)):
+        #     if s[i] == s[-(i+1)]:
+        #         r.append("t")
+        #     else:
+        #         r.append("f")
+        # if "f" in r:
+        #     return False
+        # else:
+        #     return True
+print(isPalindrome(121))
